7ae6680c8beca13df52c6e06191a84d3d640f1c0
Robert Borghese <28355157+SomeRanDev@users.noreply.github.com>
2025-05-18 02:09:44 -0400

    Use cooler × in verbose prints

--- CppCode/src/HXGUtils.cpp
+++ CppCode/src/HXGUtils.cpp
@@ -5,7 +5,7 @@
 void HXGUtils::print_verbose(const godot::String p_message) {
 #ifdef DEBUG_ENABLED
 	if(godot::is_print_verbose_enabled()) {
-		godot::print_line_rich(vformat(godot::String::utf8("『[color=orange]HAXE[/color]x[color=aqua]GODOT[/color]』 %s"), p_message));
+		godot::print_line_rich(vformat(godot::String::utf8("『[color=orange]HAXE[/color]×[color=aqua]GODOT[/color]』 %s"), p_message));
 	}
 #endif
 }
